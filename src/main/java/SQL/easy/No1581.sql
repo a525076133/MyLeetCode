8533a6d46bdc52ae32660bfef20c951219646080
wangshaocong <wangshaocong@secsmart.net>
2023-11-17 17:34:07 +0800

    2023/11/17

---
+++ src/main/java/SQL/easy/No1581.sql
@@ -0,0 +1,10 @@
+select v.customer_id customer_id, count(1) count_no_trans
+from Visits v
+    left join
+    Transactions t
+on v.visit_id = t.visit_id
+where transaction_id is null
+group by customer_id;
+
+
+
